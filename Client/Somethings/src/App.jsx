6a37f0dca1fcec95714848f9b7f27de76bd4b494
Sujan2332 <saisujan.s03@gmail.com>
2024-12-23 22:54:27 +0530

    Routes chnaged

--- Client/Somethings/src/App.jsx
+++ Client/Somethings/src/App.jsx
@@ -1,42 +1,65 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import Signup from './Something/Signup';
-import Login from './Something/Login';
-import Something from './Something/Something';
-import Post from "./Something/Post.jsx";
-import "./App.css";
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+// import Signup from './Something/Signup';
+// import Login from './Something/Login';
+// import Something from './Something/Something';
+// import Post from "./Something/Post.jsx";
+// import "./App.css";
 
-// ProtectedRoute for routes that require login
-const ProtectedRoute = ({ element }) => {
-  const navigate = useNavigate();
+// // ProtectedRoute for routes that require login
+// const ProtectedRoute = ({ element }) => {
+//   const navigate = useNavigate();
   
-  // Check if user is logged in by checking localStorage
-  const isLoggedIn = localStorage.getItem('user');
+//   // Check if user is logged in by checking localStorage
+//   const isLoggedIn = localStorage.getItem('user');
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate("/login");
-    }
-  }, [isLoggedIn, navigate]); // Run effect on isLoggedIn change
+//   useEffect(() => {
+//     if (!isLoggedIn) {
+//       navigate("/login");
+//     }
+//   }, [isLoggedIn, navigate]); // Run effect on isLoggedIn change
 
-  // If logged in, render the element
-  return isLoggedIn ? element : null;
-};
+//   // If logged in, render the element
+//   return isLoggedIn ? element : null;
+// };
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div className='App'>
+//         <Routes>
+//           {/* Protected routes */}
+//           <Route path="/" element={<ProtectedRoute element={<Something />} />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<Signup />} />
+          
+//           {/* Public route without protection */}
+//           <Route path="/post/:uniqueId" element={<Post />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Post from './Something/Post.jsx';
+import Something from './Something/Something';
+import Login from './Something/Login';
+import Signup from './Something/Signup';
 
 const App = () => {
   return (
     <Router>
-      <div className='App'>
-        <Routes>
-          {/* Protected routes */}
-          <Route path="/" element={<ProtectedRoute element={<Something />} />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          
-          {/* Public route without protection */}
-          <Route path="/post/:uniqueId" element={<Post />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Something />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/post/:uniqueId" element={<Post />} />
+      </Routes>
     </Router>
   );
 };
